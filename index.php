88c396484b960aa79d42aefd09c5b0433557ac39
Nathan PROVOST <nathan.provost78990@gmail.com>
2020-04-06 15:15:38 +0200

    Version 1.4 du site

--- index.php
+++ index.php
@@ -1,53 +1,74 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>MonSiteGitHub</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>GITHUB test</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">GIT ET GITHUB</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Accueil<span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Contact</a>
-      <a class="nav-item nav-link" href="#">A propos</a>
-    </div>
-  </div>
-</nav>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <style>
+            *{box-sizing: border-box;}
+            span{color:green; font-weight:bold;}
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid text-center bg-light">
+            <h1 class="display-4 pb-4 mb-4">Mon GITHUB SITE</h1>
+            <div class="row m-4 p-4">
+                <div class="col-md-8">
+                    <div class="row mx-0">
+                        <h2>Titre 2</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>HEADER</span><a href="#" class="mr-4 pr-4">Lien 2</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row mx-0">
+                        <h2>Titre 1</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 1</a>
+                    </div>
+                </div>
 
-<section style="min-height: 40vh;">
-    <h1 class="text-center">GROUPE GIT SUR PROJET</h1>
-    <div class="col-md-4">
-      <div class="row mx-0">
-          <h2>Titre 2</h2>
-      </div>
-      <div class="row mx-0 mt-4">
-          <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
-      </div>
-      <div class="row mx-0 d-flex justify-content-between my-2">
-          <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 2</a>
-      </div>
-    </div>
-
-
-</section>
-
-
-<footer class="bg-dark text-center p-3 text-white" >
-    <p>Copyright 2020 - Projet Groupe</p>
-</footer>
-    
-
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-</body>
+                <div class="col-md-3">
+                    <div class="row mx-0">
+                        <h2>Titre 4</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 3</a>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="row mx-0">
+                        <h2>Titre 3</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>HEADER</span><a href="#" class="mr-4 pr-4">Lien 4</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="col-md-6 bg-success my-4 display-4">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
 </html>
